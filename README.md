2c78e6aa72e4b7c5d04cbcefcaf1bcf9c584c228
payelbera <60781680+payelbera@users.noreply.github.com>
2020-10-12 22:59:44 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Virtual-pet1-boiler-plate-code-for-students
+# afa156b839e1b81fbfa9bf61b99ed6eb
---
+++ sketch.js
@@ -0,0 +1,46 @@
+//Create global variables dog, happyDog, database, foodS, foodStock
+
+function preload(){
+   //Load dog and happy dog image
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(500,500);
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.15;
+
+  //Fetch the foodStock from the database you have created using the following syntax.
+  //see hint in point #5 in pdf
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+  //Using UP_ARROW write the code to feed the dog.
+  // call writeStock() inside it
+//After feeding the dog, change the image of the dog to a happy image of the dog.
+  drawSprites();
+  //Use textSize to increase the size of the text, fill() to set text color and stroke() to outline the text.
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
+
+//Write a function to read  foodStock from the database.
+
+
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    //Deduct the count of food left from the firebase.
+  } 
+  database.ref('/').update({
+    Food:x
+  })
+}
